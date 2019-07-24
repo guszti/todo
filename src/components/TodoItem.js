3d47bef31334c1cb5ae5ee37174a47ddac73e3ac
Kiraly <agoston.kiraly@sap.com>
2019-07-24 12:10:09 +0200

    application is complete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,10 +5,11 @@ class TodoItem extends React.Component{
       return(
         <div>         
           {this.props.todoItem.checked ? <input type='checkbox' onChange={this.props.handleCheck.bind(this, this.props.id)} checked></input> : <input type='checkbox' onChange={this.props.handleCheck.bind(this, this.props.id)}></input>}
-          {this.props.todoItem.checked ? <del>   {this.props.todoItem.title} | {this.props.todoItem.date}</del> : '   ' + this.props.todoItem.title + '|' + this.props.todoItem.date}
+          {this.props.todoItem.checked ? <del>   {this.props.todoItem.title}</del> : '   ' + this.props.todoItem.title}
           <button onClick={this.props.handleDelete.bind(this, this.props.id)} style={{float:'right',height:27,width:27,color:'yellow',backgroundColor:'red'}}>X</button>
           <button onClick={this.props.itemDown.bind(this, this.props.id)} style={{float:'right',height:27,width:27,color:'yellow',backgroundColor:'blue'}}>-</button>
           <button onClick={this.props.itemUp.bind(this, this.props.id)} style={{float:'right',height:27,width:27,color:'yellow',backgroundColor:'blue'}}>+</button>
+          <small style={{float:'right',paddingRight:'10px',fontSize:12,paddingTop:'7px'}}>{this.props.todoItem.date}</small>
         </div>
       );
     }
